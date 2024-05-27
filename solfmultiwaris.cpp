e5fc5ea90a648bcc8a60dd0e0d83c7d73d546741
zackynz <nurasfanz@gmail.com>
2024-05-27 15:17:31 +0700

    mendeklarasikan class pekerja dengan metode public untuk orang

---
+++ solfmultiwaris.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
+
+class pekerja : public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
