ec01d6bd6c3a77e743744686b3a00718c5b02c79
zackynz <nurasfanz@gmail.com>
2024-05-27 15:01:23 +0700

    mendeklarasikan class baseclass final

---
+++ nopewarisan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
