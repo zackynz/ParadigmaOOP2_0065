8623949e154174f9bb88986ada80a027a04a4b04
zackynz <nurasfanz@gmail.com>
2024-05-27 16:04:58 +0700

    metode fungsi pada remote lampu rumah

--- encapsulition.cpp
+++ encapsulition.cpp
@@ -13,3 +13,18 @@ public:
         }
 };
 
+int main() {
+    remoteLampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
