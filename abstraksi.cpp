04196df5de3b17c9a03825994d74be10a2368193
zackynz <nurasfanz@gmail.com>
2024-05-27 15:41:45 +0700

    menggunakan variabel void dengan fungsi void untuk mengset variabel x dan y dan menampilkan x dan y pada display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,3 +4,14 @@ using namespace std;
 class AbstraksiKlas {
 private: string x, y;
 
+public:
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
